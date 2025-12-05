48a3ddd5277a2ca7f986f04cee294af6eb3937a8
universalShaC <universalshac@gmail.com>
2025-12-05 11:41:12 +0800

    Update q4.py
    Implement string_reverse function and add sample function calls

--- src/q4.py
+++ src/q4.py
@@ -6,9 +6,20 @@ def string_reverse(s):
     - Return the reversed string.
     """
     return
+    # Make sure s is a string
+    if not isinstance(s, str):
+        raise TypeError("s must be a string")
 
+    # Reverse the string using slicing
+    reversed_s = s[::-1]
+
+    # Return the reversed string
+    return reversed_s
 
 # Task 2
 # Invoke the function "string_reverse" using the following scenarios:
 # - "Hello World"
 # - "Python"
+
+print(string_reverse("Hello World"))
+print(string_reverse("Python"))
